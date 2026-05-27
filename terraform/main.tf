963c86f278b325457d6f1b0eb4dad5b9030deb7b
Taynan Mina Muniz <tmmuniz92@gmail.com>
2026-05-27 15:24:16 -0300

    Remove all

--- terraform/main.tf
+++
@@ -1,3 +0,0 @@
-# This demo project intentionally avoids AWS data sources.
-# The GitHub Actions workflow generates a local Terraform plan only,
-# then audits that plan with OPA/Rego without deploying infrastructure.
